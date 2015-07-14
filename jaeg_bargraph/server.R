cebbb377739452bc16d52623848ac270b363ad28
Egg Benny <beneditochou@gmail.com>
2015-07-14 00:19:15 -0700

    Subset the data to 2011 to 2014 only

--- jaeg_bargraph/server.R
+++ jaeg_bargraph/server.R
@@ -14,13 +14,21 @@ library(shiny)
 # ---- Import data ----
 
 # Import the cleaned lost and found animals data from the City of Vancouver
-lostAnimal <- read_csv("www/VancouverLostAnimals.csv")
+lostAnimal <- read_csv("www/lostAnimals.csv")
+
+# Create a reduced subset for 2011 to 2014 (only run once)
+# lostAnimal <- read_csv("www/VancouverLostAnimals.csv")
 
 # Create date variables
-lostAnimal <- lostAnimal %>% mutate(Year = factor(year(Date)),
-                                    Month = factor(month(Date)),
-                                    Day = day(Date),
-                                    Wday = wday(Date, label=TRUE))
+# lostAnimal <- lostAnimal %>% 
+#               filter(Date >=  as.Date("2011-01-01"), Date < as.Date("2015-01-01")) %>%
+#               mutate(Year = factor(year(Date)),
+#                                     Month = factor(month(Date)),
+#                                     Day = day(Date),
+#                                     Wday = wday(Date, label=TRUE))
+#               
+# 
+# write.csv(lostAnimal, "www/LostAnimals.csv")
 
 function(input, output) {
  
@@ -100,57 +108,57 @@ function(input, output) {
     })
   })
   
-  #----  Make multi-group bar graph in ggplot2 ----
-  
-  # see https://groups.google.com/forum/#!topic/shiny-discuss/Fgpd0LoZ-y8
-  # Adjust plot height using reactive fn
-  plotheight<- reactive({
-    if (input$varfacet!="") {plotheight = 1200} 
-    else {plotheight = 400} # don't use auto, it makes quantum plot! not sure why >.<
-  })
-  
-  output$plot_mg <- renderPlot({
-    # Add a progress message
-    withProgress({
-      setProgress(message = "Making a Plot")
-      # Aggregate the data differently if facet
-      if (input$varfacet != "") {
-        data_mg <- plyr::ddply(lostAnimal, c(input$varx, input$varfill, input$varfacet), plyr::summarise, count=length(State), .drop=FALSE) %>%
-          mutate(rank = rank(-count)) %>%
-          arrange(desc(count))
-        if (input$varx == "Sex" | input$varfill == "Sex") {
-          data_mg <- filter(data_mg, Sex != "X" & Sex != "")  # exclude animals with unknown sex
-        } else {data_mg}
-      } else {
-        data_mg <- plyr::ddply(lostAnimal, c(input$varx, input$varfill), plyr::summarise, count=length(State), .drop=FALSE) %>%
-          mutate(rank = rank(-count)) %>%
-          arrange(desc(count))
-        if (input$varx == "Sex" | input$varfill == "Sex") {
-          data_mg <- filter(data_mg, Sex != "X" & Sex != "")  # exclude animals with unknown sex
-        } else {data_mg}
-      }
-      
-      gg <- ggplot(data_mg, aes_string(x = input$varx, y = "count", fill = input$varfill)) + 
-        geom_bar(stat="identity", position=input$type) +
-        ylab("Count") + xlab(input$varx)
-      
-      # Add facet
-      # Use as.formula so that it will work with string var input
-      if (input$varfacet!="") {gg <- gg + facet_grid(as.formula(paste(input$varfacet,"~.")))}
-      
-      # Branch to choose graph style
-      if(input$graphstyle == 1) {gg + theme_classic()}
-      else if (input$graphstyle == 2) {gg + theme_economist() + scale_fill_economist()}
-      else if (input$graphstyle == 3) {gg + theme_excel() + scale_fill_excel()}
-      else if (input$graphstyle == 4) {gg + theme_few() + scale_fill_few()}
-      else if (input$graphstyle == 5) {gg + theme_fivethirtyeight() + 
-          scale_fill_fivethirtyeight() + theme(strip.background = element_rect(fill="#2CA02C"),
-                                               strip.text = element_text(color="white"))}
-      else if (input$graphstyle == 6) {gg + theme_stata() + scale_fill_stata()}
-      else if (input$graphstyle == 7) {gg + theme_fivethirtyeight() + 
-          scale_fill_tableau("colorblind10") + theme(strip.background = element_rect(fill="#FFBC79"))}
-      else if (input$graphstyle == 8) {gg + theme_tufte()}
-    })
-  }, height = plotheight) # adjust plot height using the reactive fn defined earlier
-  
+#   #----  Make multi-group bar graph in ggplot2 ----
+#   
+#   # see https://groups.google.com/forum/#!topic/shiny-discuss/Fgpd0LoZ-y8
+#   # Adjust plot height using reactive fn
+#   plotheight<- reactive({
+#     if (input$varfacet!="") {plotheight = 1200} 
+#     else {plotheight = 400} # don't use auto, it makes quantum plot! not sure why >.<
+#   })
+#   
+#   output$plot_mg <- renderPlot({
+#     # Add a progress message
+#     withProgress({
+#       setProgress(message = "Making a Plot")
+#       # Aggregate the data differently if facet
+#       if (input$varfacet != "") {
+#         data_mg <- plyr::ddply(lostAnimal, c(input$varx, input$varfill, input$varfacet), plyr::summarise, count=length(State), .drop=FALSE) %>%
+#           mutate(rank = rank(-count)) %>%
+#           arrange(desc(count))
+#         if (input$varx == "Sex" | input$varfill == "Sex") {
+#           data_mg <- filter(data_mg, Sex != "X" & Sex != "")  # exclude animals with unknown sex
+#         } else {data_mg}
+#       } else {
+#         data_mg <- plyr::ddply(lostAnimal, c(input$varx, input$varfill), plyr::summarise, count=length(State), .drop=FALSE) %>%
+#           mutate(rank = rank(-count)) %>%
+#           arrange(desc(count))
+#         if (input$varx == "Sex" | input$varfill == "Sex") {
+#           data_mg <- filter(data_mg, Sex != "X" & Sex != "")  # exclude animals with unknown sex
+#         } else {data_mg}
+#       }
+#       
+#       gg <- ggplot(data_mg, aes_string(x = input$varx, y = "count", fill = input$varfill)) + 
+#         geom_bar(stat="identity", position=input$type) +
+#         ylab("Count") + xlab(input$varx)
+#       
+#       # Add facet
+#       # Use as.formula so that it will work with string var input
+#       if (input$varfacet!="") {gg <- gg + facet_grid(as.formula(paste(input$varfacet,"~.")))}
+#       
+#       # Branch to choose graph style
+#       if(input$graphstyle == 1) {gg + theme_classic()}
+#       else if (input$graphstyle == 2) {gg + theme_economist() + scale_fill_economist()}
+#       else if (input$graphstyle == 3) {gg + theme_excel() + scale_fill_excel()}
+#       else if (input$graphstyle == 4) {gg + theme_few() + scale_fill_few()}
+#       else if (input$graphstyle == 5) {gg + theme_fivethirtyeight() + 
+#           scale_fill_fivethirtyeight() + theme(strip.background = element_rect(fill="#2CA02C"),
+#                                                strip.text = element_text(color="white"))}
+#       else if (input$graphstyle == 6) {gg + theme_stata() + scale_fill_stata()}
+#       else if (input$graphstyle == 7) {gg + theme_fivethirtyeight() + 
+#           scale_fill_tableau("colorblind10") + theme(strip.background = element_rect(fill="#FFBC79"))}
+#       else if (input$graphstyle == 8) {gg + theme_tufte()}
+#     })
+#   }, height = plotheight) # adjust plot height using the reactive fn defined earlier
+#   
 }
