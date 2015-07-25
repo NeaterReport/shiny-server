825d03cfba6cbbaa072fc05f374a9ee41c56f337
Egg Benny <beneditochou@gmail.com>
2015-07-24 20:11:28 -0700

    change yet again!

--- jaeg_waittime/server.R
+++ jaeg_waittime/server.R
@@ -422,9 +422,9 @@ function(input, output, session) {
       # temporarily switch to the temp dir, in case you do not have write
       # permission to the current working directory
       #owd <- setwd(tempdir())
-      owd <- setwd("www")
+      owd <- setwd("output")
       on.exit(setwd(owd))
-       file.copy(src, 'output/sampleReport.Rmd')
+       file.copy(src, 'sampleReport.Rmd')
        # file.copy(src, './www/CIHI treatment wait time.xlsx')
       
       library(rmarkdown)
